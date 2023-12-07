9b6b0a06e51093200037b14cbba27a062c5ec705
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2023-12-07 16:01:09 +0800

    Progress: Finish CRUD Seris

--- resources/views/servis/check.blade.php
+++ resources/views/servis/check.blade.php
@@ -26,7 +26,7 @@
                     method="POST">
                     @csrf
                     <div class="row">
-                        <div class="col-md-6 mb-5">
+                        <div class="col-md-6 mb-4">
                             <div class="input-wrapper">
                                 <div class="wrapper d-flex gap-3 align-items-end">
                                     <img src="{{ asset('assets/img/kendaraan-images/' . $kendaraan->foto_kendaraan) }}"
@@ -36,23 +36,23 @@
                         </div>
                         <div class="col-12">
                             <div class="row">
-                                <div class="col-md-6 mb-4">
-                                    <div class="input-wrapper">
-                                        <label for="nomor_plat">Nomor Plat</label>
+                                <div class="col-md-6 mb-3">
+                                    <div class="input-group">
+                                        <p for="nomor_plat">Nomor Plat</p>
                                         <input type="text" id="nomor_plat" class="input" autocomplete="off" disabled
                                             value="{{ $kendaraan->nomor_plat }}">
                                     </div>
                                 </div>
-                                <div class="col-md-6 mb-4">
-                                    <div class="input-wrapper">
-                                        <label for="kategori_kilometer">Kategori Kilometer</label>
+                                <div class="col-md-6 mb-3">
+                                    <div class="input-group">
+                                        <p for="kategori_kilometer">Kategori Kilometer</p>
                                         <input type="text" id="kategori_kilometer" class="input" autocomplete="off"
                                             disabled value="{{ $kendaraan->kilometer_kendaraan->jumlah }}">
                                     </div>
                                 </div>
-                                <div class="col-md-6 mb-4">
-                                    <div class="input-wrapper">
-                                        <label for="kilometer_sebelum">Kilometer Sebelum</label>
+                                <div class="col-md-6 mb-3">
+                                    <div class="input-group">
+                                        <p for="kilometer_sebelum">Kilometer Sebelum</p>
                                         <input type="number" id="kilometer_sebelum" name="kilometer_sebelum" class="input"
                                             autocomplete="off" value="{{ $kendaraan->kilometer }}" required>
                                         @error('kilometer_sebelum')
@@ -60,9 +60,9 @@
                                         @enderror
                                     </div>
                                 </div>
-                                <div class="col-md-6 mb-4">
-                                    <div class="input-wrapper">
-                                        <label for="kilometer_setelah">Kilometer Setelah</label>
+                                <div class="col-md-6 mb-3">
+                                    <div class="input-group">
+                                        <p for="kilometer_setelah">Kilometer Setelah</p>
                                         <input type="number" id="kilometer_setelah" name="kilometer_setelah" class="input"
                                             autocomplete="off" value="{{ $kendaraan->kilometer_saat_ini }}" required>
                                         @error('kilometer_setelah')
@@ -70,9 +70,9 @@
                                         @enderror
                                     </div>
                                 </div>
-                                <div class="col-md-6 mb-4">
-                                    <div class="input-wrapper">
-                                        <label for="tanggal_servis">Tanggal Servis</label>
+                                <div class="col-md-6 mb-3">
+                                    <div class="input-group">
+                                        <p for="tanggal_servis">Tanggal Servis</p>
                                         <input type="date" id="tanggal_servis" name="tanggal_servis" class="input"
                                             autocomplete="off" value="{{ old('tanggal_servis') }}" required>
                                         @error('tanggal_servis')
@@ -80,9 +80,9 @@
                                         @enderror
                                     </div>
                                 </div>
-                                <div class="col-md-6 mb-4">
-                                    <div class="input-wrapper">
-                                        <label for="total_bayar">Total Bayar</label>
+                                <div class="col-md-6 mb-3">
+                                    <div class="input-group">
+                                        <p for="total_bayar">Total Bayar</p>
                                         <input type="number" id="total_bayar" class="input" autocomplete="off"
                                             name="total_bayar" value="{{ old('total_bayar') }}">
                                         @error('total_bayar')
@@ -90,9 +90,9 @@
                                         @enderror
                                     </div>
                                 </div>
-                                <div class="col-md-6 mb-4">
-                                    <div class="input-wrapper">
-                                        <label for="air_accu">Air Accu</label>
+                                <div class="col-md-6 mb-3">
+                                    <div class="input-group">
+                                        <p for="air_accu">Air Accu</p>
                                         <select id="air_accu" class="input" name="air_accu" required>
                                             <option value="">Pilih kondisi air accu</option>
                                             <option value="ada" {{ old('air_accu') == 'ada' ? 'selected' : '' }}>Ada
@@ -107,9 +107,9 @@
                                         @enderror
                                     </div>
                                 </div>
-                                <div class="col-md-6 mb-4">
-                                    <div class="input-wrapper">
-                                        <label for="air_waiper">Air Waiper</label>
+                                <div class="col-md-6 mb-3">
+                                    <div class="input-group">
+                                        <p for="air_waiper">Air Waiper</p>
                                         <select id="air_waiper" class="input" name="air_waiper" required>
                                             <option value="">Pilih kondisi air waiper</option>
                                             <option value="ada" {{ old('air_waiper') == 'ada' ? 'selected' : '' }}>Ada
@@ -124,9 +124,9 @@
                                         @enderror
                                     </div>
                                 </div>
-                                <div class="col-md-6 mb-4">
-                                    <div class="input-wrapper">
-                                        <label for="ban">Ban</label>
+                                <div class="col-md-6 mb-3">
+                                    <div class="input-group">
+                                        <p for="ban">Ban</p>
                                         <select id="ban" class="input" name="ban" required>
                                             <option value="">Pilih kondisi ban</option>
                                             <option value="ada" {{ old('ban') == 'ada' ? 'selected' : '' }}>Ada
@@ -141,9 +141,9 @@
                                         @enderror
                                     </div>
                                 </div>
-                                <div class="col-md-6 mb-4">
-                                    <div class="input-wrapper">
-                                        <label for="oli">Oli</label>
+                                <div class="col-md-6 mb-3">
+                                    <div class="input-group">
+                                        <p for="oli">Oli</p>
                                         <select id="oli" class="input" name="oli" required>
                                             <option value="">Pilih kondisi oli</option>
                                             <option value="ada" {{ old('oli') == 'ada' ? 'selected' : '' }}>Ada
@@ -158,9 +158,9 @@
                                         @enderror
                                     </div>
                                 </div>
-                                <div class="col-12 row-button">
-                                    <div class="input-wrapper">
-                                        <label for="keterangan">Keterangan</label>
+                                <div class="col-12 mb-4">
+                                    <div class="input-group">
+                                        <p for="keterangan">Keterangan</p>
                                         <input type="text" id="keterangan" class="input" autocomplete="off"
                                             name="keterangan" value="{{ old('keterangan') }}">
                                         @error('keterangan')
